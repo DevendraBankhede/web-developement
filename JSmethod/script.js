f6ce60f4a57c8c4479facadbdd036d97d1cc7608
HodManagmentSystem <harshubankhede@gmail.com>
2026-05-01 23:23:55 +0530

    jsMethod

---
+++ JSmethod/script.js
@@ -0,0 +1,6 @@
+let a = 20,b = 30;
+
+function sum(x,y){
+    let z = x+y
+    return z;
+};
---
+++ JSmethod/script2.js
@@ -0,0 +1,20 @@
+const abc = function(){
+    console.log("Hello World");
+};
+
+console.log(abc);
+
+abc();
+
+
+
+const arrowfun = () => {
+    console.log("I am Arrow Function");
+};
+
+arrowfun();
+
+
+const addarrow = (a,b) => a+b;
+
+console.log(addarrow(5,6));
