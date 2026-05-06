e358d808e4216c69e1e23e417dba8a328d4befca
HodManagmentSystem <harshubankhede@gmail.com>
2026-05-06 18:00:32 +0530

    JsAssignment-3

--- Assingment/Assingment5-(js)/script.js
+++ Assingment/Assingment5-(js)/script.js
@@ -15,3 +15,41 @@ function Submit() {
     document.getElementById("dataCard").classList.add("divShow");
     document.getElementById("dataCard").classList.remove("divHide");
 }
+
+function  Submit() {
+    console.log("Submit Button Clicked");
+
+    const Email = document.getElementById("Email").value;
+    const Password = document.getElementById("Password").value;
+    console.log(Email);
+    console.log(Password);
+
+    document.getElementById("myEmail").innerText = Email;
+    document.getElementById("Email").value = "";
+
+    document.getElementById("myPassword").innerText = Password;
+    document.getElementById("Password").value = "";
+
+    document.getElementById("dataCard").classList.add("divShow");
+    document.getElementById("dataCard").classList.remove("divHide");
+}
+
+
+function Submit() {
+    console.log("Submit Button Clicked");
+
+    const FirstName = document.getElementById("FirstName").value;
+    const LastName = document.getElementById("LastName").value;
+    console.log(FirstName);
+    console.log(LastName);
+
+
+    document.getElementById("myFirstName").innerText = FirstName;
+    document.getElementById("FirstName").value = "";
+
+    document.getElementById("myLastName").innerText = LastName;
+    document.getElementById("LastName").value = "";
+
+    document.getElementById("dataCard").classList.add("divShow");
+    document.getElementById("dataCard").classList.remove("divHide");
+}
