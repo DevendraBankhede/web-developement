54c8d91635d1f713075440ee4f1bc88fa733672c
HodManagmentSystem <harshubankhede@gmail.com>
2026-05-16 15:30:14 +0530

    JavaScript Variables,Operaters,Conditons,Loop,Functions precties

---
+++ jsPracties/script.js
@@ -0,0 +1,38 @@
+// Variable
+let Name = "Devendra";
+let age = 20;
+let isStudent = true;
+
+console.log(Name);
+
+
+// operators
+let a = 10;
+let b = 5;
+
+console.log(a+b);
+console.log(a-b);
+
+// Conditions (if-else)
+
+    let Age = 18;
+
+    if(Age>=18)
+    {
+        console.log("Adult");
+    }
+    else{
+        console.log("NoAdult");
+    }
+
+    // loop
+for(let i=1;i<=5;i++)
+{
+    console.log(i);
+}
+
+// Functions
+function greet(){
+    console.log("Hello");
+}
+greet();
